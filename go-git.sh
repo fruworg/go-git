e09af1ac3a530ba4df8202c4e6f0a88ae43cf9b8
Mitch Conner <96399261+fruworg@users.noreply.github.com>
2022-01-18 20:43:22 +0500

    Update go-git.sh

--- go-git.sh
+++ go-git.sh
@@ -1,9 +1,36 @@
 #!/bin/bash
-username=""
-if [[ "$username" != "" ]]; then
-echo Введи название проекта:
+if [ ! -f username  ]; then
+echo Введи ник:
+read username
+echo Введи токен:
+read token
+echo "$token" > token
+echo "$username" > username
+else
+token=$(cat token)
+username=$(cat username)
+fi
+echo Введи имя репозитория:
 read repo
-
+title="
+username="$username"
+repo="$repo"
+token="$token"
+"
+commit='
+gh auth login --with-token < ../token > /dev/null
+if (! gh repo view "$repo" 1> /dev/null 2> /dev/null); then
+gh repo create "$repo" --public 1> /dev/null
+fi
+git init 1> /dev/null 2> /dev/null
+git add . 
+git rm --cached commit-"$repo".sh 1> /dev/null
+echo Введи имя коммита:
+read commit
+git commit -m "$commit"
+git branch -M main
+git push --set-upstream https://"$token"@github.com/"$username"/"$repo".git main
+'
 main='package main
 
 import (
@@ -23,10 +50,9 @@ require (
 "
 mkdir "$repo"
 cd "./$repo"
-echo "###$repo" > README.md
+echo "### $repo" > README.md
 echo "$main" > main.go
 echo "$mod" > mod.go
-cd ..
-else 
-echo Измени конфиг!
-fi
+echo "$title" > commit-"$repo".sh
+echo "$commit" >> commit-"$repo".sh
+chmod 775 commit-"$repo".sh
